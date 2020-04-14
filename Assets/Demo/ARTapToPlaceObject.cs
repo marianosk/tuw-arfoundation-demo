8992fdb3bf921d70eff5b89a5a65b9692ced1060
Marián Stach <marianosk@users.noreply.github.com>
2020-04-14 07:48:26 +0200

    pokus1

--- Assets/Demo/ARTapToPlaceObject.cs
+++ Assets/Demo/ARTapToPlaceObject.cs
@@ -4,64 +4,94 @@ using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.Experimental.XR;
 using System;
+using UnityEngine.XR.ARSubsystems;
 
 public class ARTapToPlaceObject : MonoBehaviour
 {
-    public GameObject objectToPlace;
+    //public GameObject objectToPlace;
     public GameObject placementIndicator;
 
-    private ARSessionOrigin arOrigin;
+    //private ARSessionOrigin arOrigin;
+    private ARRaycastManager aRRaycastManager;
     private Pose placementPose;
     private bool placementPoseIsValid = false;
 
     void Start()
     {
-        arOrigin = FindObjectOfType<ARSessionOrigin>();
+        Debug.Log("@@@@@@@@@@@@@@@@ START @@@@@@@@@@@@@@@@@");
+        //arOrigin = FindObjectOfType<ARSessionOrigin>();
+        aRRaycastManager = FindObjectOfType<ARRaycastManager>();
+        Debug.Log("RayCastManager = " + aRRaycastManager.tag);
     }
 
     void Update()
     {
+        Debug.Log("Update start");
         UpdatePlacementPose();
         UpdatePlacementIndicator();
 
-        if (placementPoseIsValid && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
-        {
-            PlaceObject();
-        }
+        //if (placementPoseIsValid && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        //{
+        //    PlaceObject();
+        //}
     }
 
-    private void PlaceObject()
-    {
-        Instantiate(objectToPlace, placementPose.position, placementPose.rotation);
-    }
+    //private void PlaceObject()
+    //{
+    //    Instantiate(objectToPlace, placementPose.position, placementPose.rotation);
+    //}
 
     private void UpdatePlacementIndicator()
     {
         if (placementPoseIsValid)
         {
+            Debug.Log("placement indicator is valid");
             placementIndicator.SetActive(true);
             placementIndicator.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
         }
         else
         {
+            Debug.Log("placement indicator is NOT valid");
             placementIndicator.SetActive(false);
         }
     }
 
     private void UpdatePlacementPose()
     {
-        var screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
-        var hits = new List<ARRaycastHit>();
-        arOrigin.Raycast(screenCenter, hits, TrackableType.Planes);
-
-        placementPoseIsValid = hits.Count > 0;
-        if (placementPoseIsValid)
+        if(Camera.current != null)
         {
-            placementPose = hits[0].pose;
+            Debug.Log("Camera.current = " + Camera.current.ToString());
+        } else
+        {
+            Debug.Log("Camera.current = null");
+        }
+        
+        var screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
 
-            var cameraForward = Camera.current.transform.forward;
-            var cameraBearing = new Vector3(cameraForward.x, 0, cameraForward.z).normalized;
-            placementPose.rotation = Quaternion.LookRotation(cameraBearing);
+        Debug.Log("Screen center = " + screenCenter.ToString());
+
+        var hits = new List<ARRaycastHit>();
+
+        if (aRRaycastManager != null)
+        {
+            Debug.Log("aRRayCatManager is OK");
+            aRRaycastManager.Raycast(screenCenter, hits, TrackableType.Planes);
+            //arOrigin.camer Raycast(screenCenter, hits, TrackableType.Planes);
+
+            Debug.Log("Hits = " + hits.Count);
+
+            placementPoseIsValid = hits.Count > 0;
+            if (placementPoseIsValid)
+            {
+                placementPose = hits[0].pose;
+
+                var cameraForward = Camera.current.transform.forward;
+                var cameraBearing = new Vector3(cameraForward.x, 0, cameraForward.z).normalized;
+                placementPose.rotation = Quaternion.LookRotation(cameraBearing);
+            }
+        } else
+        {
+            Debug.Log("aRRayCatManager is NULL");
         }
     }
 }
